9b1a1c3cd401f8aa87a4579ef7e81d9f92fec1aa
mad <mad@jangjicheol.local>
2023-02-23 14:50:17 +0900

    2.Interduce observer로 animation 구현

--- src/components/contents/2.Introduce.tsx
+++ src/components/contents/2.Introduce.tsx
@@ -9,41 +9,71 @@ import adobe1 from 'img/adobe1.png';
 import adobe2 from 'img/adobe2.png';
 import adobe3 from 'img/adobe3.png';
 import me from 'img/me.jpeg';
+import { useEffect, useState, useRef } from 'react';
 
 export default function Introduce() {
+    const [animateState, setAnimateState] = useState({
+        name: false,
+        phone: false,
+        email: false,
+        address: false,
+        education: false,
+        career: false,
+        skill: false,
+    });
+
+    const cardItems = useRef<any>([]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((items) => {
+            items.forEach((item) => {
+                if (item.isIntersecting && item.intersectionRatio > 0.9) {
+                    setAnimateState((prev) => {
+                        return {...prev, [item.target.id]: true}
+                    })
+                } else {
+                    setAnimateState((prev) => {
+                        return {...prev, [item.target.id]: false}
+                    })
+                }
+            })
+        })
+        cardItems.current.forEach((e: HTMLDivElement) => {observer.observe(e)})
+    })
+    
     return (
         <div className='introduce'>
             <div className='introduce__card about-me'>
-                <div className="card__item">
+                <div ref={e => cardItems.current[0] = e} id='name' className={animateState.name ? 'card__item animate' : 'card__item'}>
                     <div className='item__me'><img src={me} alt="me" /></div>
                     <div className="item__name-birth">
                         <span className='item__name'>장 지 철</span>
                         <span className='item__birth'>1992 0812</span>
                     </div>
                 </div>
-                <div className="card__item">
+                <div ref={e => cardItems.current[1] = e} id='phone' className={animateState.phone ? 'card__item animate' : 'card__item'}>
                     <span className='item__key'>PHONE NUMBER</span>
                     <span className='item__value'>010-6765-2325</span>
                 </div>
-                <div className="card__item">
+                <div ref={e => cardItems.current[2] = e} id='email' className={animateState.email ? 'card__item animate' : 'card__item'}>
                     <span className='item__key'>E-MAIL</span>
                     <span className='item__value'>wkdwlcjf1234@gmail.com</span>
                 </div>
-                <div className="card__item">
+                <div ref={e => cardItems.current[3] = e} id='address' className={animateState.address ? 'card__item animate' : 'card__item'}>
                     <span className='item__key'>ADDRESS</span>
                     <span className='item__value'>서울특별시 양천구 신정동</span>
                 </div>
-                <div className="card__item">
+                <div ref={e => cardItems.current[4] = e} id='education' className={animateState.education ? 'card__item animate' : 'card__item'}>
                     <span className='item__key'>EDUCATION</span>
                     <span className='item__value'>가천대학교<br />사회복지학과<br />2011 ~ 2018</span>
                 </div>
                 
             </div>
 
-            <div className='introduce__card career'>
-                <div className="card__item">
+            <div className='introduce__card career-skill'>
+                <div ref={e => cardItems.current[5] = e} id='career' className={animateState.career ? 'card__item animate' : 'card__item'}>
                     <span className='item__key'>CAREER</span>
-                    <table className="card__item">
+                    <table className='item__value'>
                         <tbody>
                             <tr>
                                 <th className='year'>2020</th>
@@ -81,7 +111,8 @@ export default function Introduce() {
                             </tr>
                         </tbody>
                     </table>
-                    <div className="card__item">
+                </div>
+                <div ref={e => cardItems.current[6] = e} id='skill' className={animateState.skill ? 'card__item animate' : 'card__item'}>
                     <span className='item__key'>SKILL</span>
                     <div className="item__value">
                         <span className='item__logo-wrap-title'><i className="fa-solid fa-code icon"></i>DEVELOPMENT</span>
@@ -101,8 +132,8 @@ export default function Introduce() {
                         </ul>
                     </div>
                 </div>
-                </div>
             </div>
         </div>
     )
 }
+
