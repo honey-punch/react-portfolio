e006d2ccbd1c6e813ac6c164e8c9a914ebd34512
mad <mad@jangjicheol.local>
2023-02-23 17:28:12 +0900

    WIP 3.Projects design

--- src/components/contents/3.Projects.tsx
+++ src/components/contents/3.Projects.tsx
@@ -3,7 +3,13 @@ import 'css/contents/3.Projects.css';
 export default function Projects() {
     return (
         <div className="projects">
-            
+            <div className="projects__box">
+                <div className="box__bar">
+                    <div className="bar__btn-group">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
