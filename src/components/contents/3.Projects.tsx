81231ba2dc4954a14a60d2bee81a26467110ae31
Ji-Cheol Chang <wkdwlcjf1234@gmail.com>
2023-03-05 17:20:47 +0900

    add projects video

--- src/components/contents/3.Projects.tsx
+++ src/components/contents/3.Projects.tsx
@@ -7,6 +7,7 @@ type tabmenu = {
     key: string;
     desc: string;
     skill: string;
+    url: string;
 }
 type Detail = {
     [key: string] : boolean;
@@ -25,30 +26,35 @@ export default function Projects() {
             key: 'sofaMusic',
             desc: 'React를 이용해 만든 가상의 음악 엔터테인먼트 웹페이지',
             skill: 'React, Ts, Css',
+            url: '/videos/sofa-music.mp4',
         },
         {
             name: 'Todo List',
             key: 'todoList',
-            desc: '',
+            desc: 'React를 이용한 간단한 todo list',
             skill: 'React, Ts, Css',
+            url: '/videos/todo-list.mp4',
         },
         {
             name: 'e-Book',
             key: 'eBook',
-            desc: '',
-            skill: 'React, Ts, Css',
+            desc: 'eBook 가상 웹사이트',
+            skill: 'HTML, Tailwind, JS',
+            url: '/videos/ebook.mp4',
         },
         {
             name: 'School Music',
             key: 'schoolMusic',
-            desc: '',
-            skill: 'React, Ts, Css',
+            desc: "악기 판매사이트 '스쿨뮤직'의 리뉴얼 프로젝트",
+            skill: 'HTML, Css, JS',
+            url: '/videos/school-music.mp4',
         },
         {
             name: 'Dalock',
             key: 'dalock',
-            desc: '',
-            skill: 'React, Ts, Css',
+            desc: "셀프 짐보관 '미니창고 다락'의 리뉴얼 프로젝트",
+            skill: 'HTML, Css, JS',
+            url: '/videos/dalock.mp4',
         },
     ]
     const onClickTabmenu = (index: number) => {
@@ -95,22 +101,22 @@ export default function Projects() {
                     </ul>
                 </div>
                 {tabmenuArray.map((element, index) => (
-                    <div key={element.key}
-                        className={index === currentTab ? 'box__content active' : 'box__content'}>
-                        <div className='content__img'>
-                            <img src="#" alt="gif" />
-                        </div>
-                        <div className={currentDetail[element.key] ? 'content__detail active' : 'content__detail'}>
-                            <div className="detail__name">{element.name}</div>
-                            <div className="detail__desc">{element.desc}</div>
-                            <div className="detail__skill">{element.skill}</div>
-                        </div>
-                        <button className={upDownState ? 'content__detail-btn up-down': 'content__detail-btn'}
-                                onClick={() => {onClickDetail(element.key)}}
-                                >
-                                <i className="fa-solid fa-angles-up"></i>상세 설명
-                        </button>
+                <div key={element.key}
+                    className={index === currentTab ? 'box__content active' : 'box__content'}>
+                    <div className='content__video'>
+                        <video src={process.env.PUBLIC_URL + element.url} loop muted autoPlay></video>
                     </div>
+                    <div className={currentDetail[element.key] ? 'content__detail active' : 'content__detail'}>
+                        <div className="detail__name detail">{element.name}</div>
+                        <div className="detail__desc detail">{element.desc}</div>
+                        <div className="detail__skill detail">{element.skill}</div>
+                    </div>
+                    <button className={upDownState ? 'content__detail-btn up-down': 'content__detail-btn'}
+                            onClick={() => {onClickDetail(element.key)}}
+                            >
+                            <i className="fa-solid fa-angles-up"></i>상세 설명
+                    </button>
+                </div>
                 ))}
             </div>
         </div>
